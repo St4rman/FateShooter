51bde6e7877fcc8566d4c98740414c848077d42e
Eddu <55552484+St4rman@users.noreply.github.com>
2025-09-05 14:32:14 +0100

    trim

--- ProjectFate/Intermediate/Build/Win64/x64/ProjectFateEditor/Development/Engine/SharedPCH.Engine.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Engine/Public/EngineSharedPCH.h
-#include "E:/Projects/UNREAL/Fate/FateShooter/ProjectFate/Intermediate/Build/Win64/x64/ProjectFateEditor/Development/Engine/SharedDefinitions.Engine.Cpp20.h"
-#include "Runtime/Engine/Public/EngineSharedPCH.h"
--- ProjectFate/Intermediate/Build/Win64/x64/ProjectFateEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Slate/Public/SlateSharedPCH.h
-#include "E:/Projects/UNREAL/Fate/FateShooter/ProjectFate/Intermediate/Build/Win64/x64/ProjectFateEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
-#include "Runtime/Slate/Public/SlateSharedPCH.h"
