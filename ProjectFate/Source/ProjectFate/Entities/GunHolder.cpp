4db55977ff84cd358c881217412503709e44dd62
Eddu <55552484+St4rman@users.noreply.github.com>
2025-10-14 15:37:12 +0100

    didnt do this much

---
+++ ProjectFate/Source/ProjectFate/Entities/GunHolder.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GunHolder.h"
+
+
+// Sets default values
+AGunHolder::AGunHolder()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AGunHolder::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGunHolder::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
